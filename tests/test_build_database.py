82c3a2a05bd203ecb06abf6f0ca283ab55845219
Victor Onyenezido <sleonvictor@gmail.com>
2020-09-24 14:58:30 +0100

    Add test to verify the ohlc table exists

--- tests/test_build_database.py
+++ tests/test_build_database.py
@@ -2,6 +2,7 @@ import os
 import unittest
 from config import db, app, database_file # pylint: disable=F0401
 from database.build_database import build_database # pylint: disable=F0401
+import sqlite3
 
 class TestBuildDatabase(unittest.TestCase):
     def test_create_new_database_if_none_exists(self):
@@ -34,6 +35,31 @@ class TestBuildDatabase(unittest.TestCase):
         build_database()
         # Assert
         self.assertEqual(last_modified_time, os.stat(database_file).st_mtime)
+
+    def test_that_ohlc_table_exists(self):
+        """
+        GIVEN that the database file already exists
+        THEN the ohlc table should also exist
+        """
+        # Arrange
+        # Create the database file if it does not already exist
+        if not os.path.exists(database_file):
+            build_database()
+        
+        # Act
+        conn = sqlite3.connect(database_file)
+        c = conn.cursor()        
+        #get the count of tables with the name ohlc
+        c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='ohlc' ''')
+        table_count = c.fetchone()[0]
+        conn.commit()
+        #close the connection
+        conn.close()
+
+        # Assert
+        self.assertEqual(table_count, 1)
+
+
 
 if __name__ == '__main__':
     unittest.main()
